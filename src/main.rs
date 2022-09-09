9c61be2a053375b83b84b1d655fda8016852a47f
Brent Mode <brent.a.mode@gmail.com>
2022-09-09 17:28:52 -0500

    a sufficient implementation to run the IBM logo program in the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,136 @@
 #![allow(unused)]
+use std::io::{stdout, Write};
+use bitvec::prelude::*;
+use crossterm::{cursor, QueueableCommand, execute, Result, style::{self, Stylize}, terminal, terminal::size};
+
 fn main() {
-    // nibble extraction testing
-    // test with 1111_1110_1100_1000
-    // want to discard 1111
-    // nnn or addr = 1110_1100_1000
-    // n or nibble = 1000
-    // x           = 1110
-    // y           = 1100
-    // kk or byte  = 1100_1000
-    let t = 0b1111_1110_1100_1000;
-    println!("test: {:b}", t);
-    println!("opcode: {:b}", t >> 12);
+    let mut chip8 = Chip8::new();
+    let file = std::fs::read("IBM Logo.ch8").unwrap();
+    let file = file.as_slice();
+    for (i, byte) in file.iter().enumerate() {
+        chip8.cpu.mem[i + 0x200] = *byte;
+    }
+    chip8.run().unwrap();
+}
+
+#[derive(Debug)]
+struct Chip8 {
+    cpu: Cpu,
+    clock: Clock,
+    term: Terminal,
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        let cpu = Cpu::new();
+        let clock = Clock;
+        let term = Terminal;
+        Chip8 { cpu, clock, term }
+    }
+    pub fn run(&mut self) -> std::result::Result<(), TerminalError> {
+        self.term.clear_screen();
+        loop {
+            let next_inst = self.cpu.fetch_next();
+            let msg = self.cpu.execute_instruction(next_inst);
+            match msg {
+                Chip8Message::None => { },
+                Chip8Message::ClearScreen => { self.term.clear_screen()? },
+                Chip8Message::DrawScreen => { self.term.draw_screen(&self.cpu.disp)? },
+            }
+            self.clock.tick();
+        }
+    }
+}
+
+enum Chip8Message {
+    None,
+    ClearScreen,
+    DrawScreen,
+}
+
+const CLOCK_RATE: f64 = 700. ; // Hz, 700 instructions per second
+
+#[derive(Debug)]
+struct Clock;
+
+impl Clock {
+    fn tick(&self) {
+        std::thread::sleep(std::time::Duration::from_millis((1. / CLOCK_RATE * 100.).round() as u64));
+    }
+}
+
+#[derive(Debug)]
+struct Terminal;
+
+impl Terminal {
+    const MIN_WIDTH: u16 = 64;
+    const MIN_HEIGHT: u16 = 32;
+
+    fn check_bounds(&self, w: u16, h: u16) -> std::result::Result<(), TerminalError> {
+        if w < Self::MIN_WIDTH || h < Self::MIN_HEIGHT {
+            return Err(TerminalError::ErrorKind("terminal is too small to display screen".to_string()))
+        }
+        Ok(())
+    }
+}
+
+trait TerminalBackend { 
+    type Error;
+    fn clear_screen(&mut self) -> std::result::Result<(), Self::Error>;
+    fn draw_screen(&mut self, display: &[[u8; 32]; 64]) -> std::result::Result<(), Self::Error>;
+}
+
+#[derive(Clone, Debug)]
+enum TerminalError { 
+    ErrorKind(String),
+}
+
+impl std::fmt::Display for TerminalError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            TerminalError::ErrorKind(s) => writeln!(f, "error: {}", s)?,
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for TerminalError { }
+
+impl From<std::io::Error> for TerminalError {
+    fn from(err: std::io::Error) -> TerminalError {
+        dbg!(err);
+        TerminalError::ErrorKind("received an io error".to_string())
+    }
+}
+
+impl TerminalBackend for Terminal {
+    type Error = TerminalError;
+    fn clear_screen(&mut self) -> std::result::Result<(), Self::Error> { 
+        let (width, height) = size()?;
+        self.check_bounds(width, height)?;
+        execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
+        Ok(()) 
+    }
+
+    fn draw_screen(&mut self, disp: &[[u8; 32]; 64]) -> std::result::Result<(), Self::Error> { 
+        let (width, height) = size()?;
+        self.check_bounds(width, height)?;
+        let mut stdout = stdout();
+        for (i, row) in disp.iter().enumerate() {
+            for (j, pix) in row.iter().enumerate() {
+                stdout.queue(cursor::MoveTo(i as u16, j as u16))?;
+                if *pix == 0 {
+                    stdout.queue(style::PrintStyledContent("█".black()))?;
+                } else if *pix == 1 {
+                    stdout.queue(style::PrintStyledContent("█".white()))?;
+                } else {
+                    return Err(TerminalError::ErrorKind("display pixel set to value other than 0 or 1".to_string()))
+                }
+            }
+        }
+        stdout.flush();
+        Ok(()) 
+    }
 }
 
 type Memory = [u8; 4096];
@@ -25,6 +145,7 @@ type ProgramCounter = u16;
 /// All CHIP-8 programs start the program counter here.
 const START: u16 = 0x200;
 
+#[derive(Debug)]
 struct Cpu {
     mem: Memory,
     disp: Display,
@@ -52,7 +173,13 @@ impl Cpu {
         }
     }
 
-    pub fn execute_instruction(&mut self, inst: u16) {
+    pub fn fetch_next(&mut self) -> u16 {
+        let next_inst = ((self.mem[self.pc as usize] as u16) << 8) as u16 + (self.mem[self.pc as usize + 1]) as u16;
+        self.pc += 2;
+        next_inst
+    }
+
+    pub fn execute_instruction(&mut self, inst: u16) -> Chip8Message {
         let op = inst >> 12;
         let nnn = inst & 0b0000_1111_1111_1111;
         let n = inst & 0b0000_0000_0000_1111;
@@ -62,25 +189,62 @@ impl Cpu {
         let raw_op = RawOpcode::new(op, x, y, n, kk, nnn);
         let opcode = Opcode::from(&raw_op);
         match opcode {
-            Opcode::Clear => { self.clear() },
-            Opcode::Jump => { self.jump(nnn) },
-            Opcode::SetVX => { },
-            Opcode::AddVX => { },
-            Opcode::SetI => { },
-            Opcode::Draw => { },
+            Opcode::None => { Chip8Message::None },
+            Opcode::Error => { println!("cpu status: {:?}", self); panic!() }
+            Opcode::Clear => { Chip8Message::ClearScreen },
+            Opcode::Jump => { self.jump(nnn); Chip8Message::None },
+            Opcode::SetVX => { self.set_vx(x, kk); Chip8Message::None },
+            Opcode::AddVX => { self.add_vx(x, kk); Chip8Message::None },
+            Opcode::SetI => { self.set_i(nnn); Chip8Message::None },
+            Opcode::Draw => { self.draw(x, y, n); Chip8Message::DrawScreen },
         }
     }
 
-    fn clear(&mut self) {
-        // I plan to use crossterm for this
-        unimplemented!();
+    fn jump(&mut self, nnn: u16) {
+        self.pc = nnn;
     }
 
-    fn jump(&mut self, nnn: u16) {
-        self.pc == nnn;
+    fn set_vx(&mut self, x: u16, nn: u16) {
+        self.reg[x as usize] = nn as u8;
+    }
+
+    fn add_vx(&mut self, x: u16, nn: u16) {
+        self.reg[x as usize] += nn as u8;
+    }
+
+    fn set_i(&mut self, nnn: u16) {
+        self.index = nnn;
+    }
+
+    fn draw(&mut self, x: u16, y: u16, n: u16) {
+        let mut x_coord = self.reg[x as usize] % 64;
+        let start_x_coord = x_coord;
+        let mut y_coord = self.reg[y as usize] % 32;
+        self.reg[0xF as usize] = 0;
+        for i in 0..n {
+            x_coord = start_x_coord;
+            let sprite_data = self.mem[self.index as usize + i as usize];
+            for b in sprite_data.view_bits::<Msb0>().iter().by_val() {
+                if b && self.disp[x_coord as usize][y_coord as usize] == 1 {
+                    self.disp[x_coord as usize][y_coord as usize] = 0;
+                    self.reg[0xF as usize] = 1;
+                } else if b && self.disp[x_coord as usize][y_coord as usize] == 0 {
+                    self.disp[x_coord as usize][y_coord as usize] = 1;
+                }
+                if x_coord == 63 {
+                    break;
+                }
+                x_coord += 1;
+            }
+            y_coord += 1;
+            if y_coord == 31 {
+                break;
+            }
+        }
     }
 }
 
+#[derive(Debug)]
 struct RawOpcode {
     op: u16,
     x: u16,
@@ -96,6 +260,13 @@ impl RawOpcode {
     }
 }
 
+impl std::fmt::Display for RawOpcode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f, "{:x}{:x}{:x}{:x}", self.op, self.x, self.y, self.n)?;
+        Ok(())
+    }
+}
+
 enum Opcode {
     Clear, // 00E0
     Jump, // 1NNN
@@ -103,6 +274,8 @@ enum Opcode {
     AddVX, // 7XNN
     SetI, // ANNN
     Draw, // DXYN
+    None, // other
+    Error, // error
 }
 
 impl std::convert::From<&RawOpcode> for Opcode {
@@ -113,7 +286,7 @@ impl std::convert::From<&RawOpcode> for Opcode {
                 if raw_op.x == 0 && raw_op.y == 0xE && raw_op.n == 0 {
                     return Opcode::Clear 
                 } else {
-                    panic!()
+                    return Opcode::None
                 }
             },
             1 => { return Opcode::Jump },
@@ -121,7 +294,7 @@ impl std::convert::From<&RawOpcode> for Opcode {
             7 => { return Opcode::AddVX },
             0xA => { return Opcode::SetI },
             0xD => { return Opcode::Draw },
-            _ => { unimplemented!() },
+            _ => { println!("\nencountered unknown opcode: {}", raw_op); return Opcode::Error },
         }
     }
 }
